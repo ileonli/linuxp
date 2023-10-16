dfde44c6d8e645c76779b9918e8a16634ac1f96f
Leon Li <byteflow777@gmail.com>
2023-10-16 21:26:04 +0800

    add: read_all_content

--- src/file.c
+++ src/file.c
@@ -10,3 +10,33 @@ off_t retrieve_file_size(const char *name) {
     close(fd);
     return size;
 }
+
+char *read_all_content(const char *name) {
+    int fd = open(name, O_RDONLY);
+    if (fd == -1) {
+        return NULL;
+    }
+
+    char *content = malloc(sizeof(char) * BUF_SIZE);
+    char buf[BUF_SIZE];
+
+    ssize_t bytes_read, total_read = 0;
+    while ((bytes_read = read(fd, buf, BUF_SIZE)) > 0) {
+        memcpy(content + total_read, buf, bytes_read);
+        total_read += bytes_read;
+    }
+
+    if (bytes_read == -1) {
+        free(content);
+        content = NULL;
+    } else {
+        char *tmp = realloc(content, sizeof(char) * (total_read + 1));
+        if (content) {
+            tmp[total_read] = '\0';
+            content = tmp;
+        }
+    }
+
+    close(fd);
+    return content;
+}
--- src/file.h
+++ src/file.h
@@ -1,6 +1,12 @@
 #pragma once
 
+#include <memory.h>
+#include <malloc.h>
 #include <unistd.h>
 #include <fcntl.h>
 
+#define BUF_SIZE 256
+
 off_t retrieve_file_size(const char *name);
+
+char *read_all_content(const char *name);
